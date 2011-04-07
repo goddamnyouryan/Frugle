bcbe1b0adee2a0720687c5c2e39a70856046b4a9
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-04-07 00:40:04 -0700

    git push heroku master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,10 @@ class HomeController < ApplicationController
     if user_signed_in?
       if current_user.role == "business"
         @business = current_user.business
+        @map = GMap.new("map_div")
+        @map.control_init(:large_map => true,:map_type => true)
+        @map.center_zoom_init([@business.latitude,@business.longitude],14)
+        @map.overlay_init(GMarker.new([@business.latitude,@business.longitude],:title => "#{@business.name}", :info_window => "#{@business.name} <br /> #{@business.address}<br />#{@business.zip}<br />#{@business.phone}"))
         @active_frugles = Frugle.find :all, :conditions => ["business_id = ? AND status = ?", @business.id, "active"]
         @expired_frugles = Frugle.find :all, :conditions => ["business_id = ? AND status = ?", @business.id, "expired"]
       else
