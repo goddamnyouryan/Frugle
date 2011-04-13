34beaae7f645a87f32fb30a9f9d345a9f235f10e
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-04-12 23:56:50 -0700

    fixing things

--- app/controllers/categorizations_controller.rb
+++ app/controllers/categorizations_controller.rb
@@ -4,7 +4,7 @@ class CategorizationsController < ApplicationController
     if user_signed_in?
       @category = Category.find(params[:category_id])
       @categorization = Categorization.create(:user_id => current_user.id, :category_id => params[:category_id])
-      @businesses = Business.find :all, :include => :frugles, :conditions => ["category_id = ? AND neighborhood_id = ? AND frugles.id IS NOT NULL", @category.id, current_user.neighborhood_id]
+      @businesses = Business.find :all, :include => :frugles, :conditions => ["businesses.category_id = ? AND neighborhood_id = ? AND frugles.id IS NOT NULL", @category.id, current_user.neighborhood_id]
       unless @businesses.empty?
         @subcategories = @businesses.map(&:subcategory).uniq
         @subcategories.each do |s|
@@ -38,7 +38,7 @@ class CategorizationsController < ApplicationController
       @user = User.find_by_logged_out("#{@session_id}")
       @category = Category.find(params[:category_id])
       @categorization = Categorization.create(:user_id => @user.id, :category_id => params[:category_id])
-      @businesses = Business.find :all, :include => :frugles, :conditions => ["category_id = ? AND neighborhood_id = ? AND frugles.id IS NOT NULL", @category.id, session[:neighborhood]]
+      @businesses = Business.find :all, :include => :frugles, :conditions => ["businesses.category_id = ? AND neighborhood_id = ? AND frugles.id IS NOT NULL", @category.id, session[:neighborhood]]
       unless @businesses.empty?
       @subcategories = @businesses.map(&:subcategory).uniq
       @subcategories.each do |s|
@@ -75,7 +75,7 @@ class CategorizationsController < ApplicationController
       @categorization = Categorization.find_by_user_id_and_category_id(current_user.id, params[:category_id])
       @categorization.destroy
       @category = Category.find(params[:category_id])
-      @businesses = Business.find :all, :include => :frugles, :conditions => ["category_id = ? AND frugles.id IS NOT NULL", @category.id]
+      @businesses = Business.find :all, :include => :frugles, :conditions => ["businesses.category_id = ? AND frugles.id IS NOT NULL", @category.id]
       @subcategories = @businesses.map(&:subcategory).uniq
       @subcategories.each do |s|
           @subcategorization = Subcategorization.find_by_user_id_and_subcategory_id(current_user.id, s.id)
@@ -111,7 +111,7 @@ class CategorizationsController < ApplicationController
       @categorization = Categorization.find_by_user_id_and_category_id(@user.id, params[:category_id])
       @categorization.destroy
       @category = Category.find(params[:category_id])
-      @businesses = Business.find :all, :include => :frugles, :conditions => ["category_id = ? AND frugles.id IS NOT NULL", @category.id]
+      @businesses = Business.find :all, :include => :frugles, :conditions => ["businesses.category_id = ? AND frugles.id IS NOT NULL", @category.id]
       @subcategories = @businesses.map(&:subcategory).uniq
       @subcategories.each do |s|
           @subcategorization = Subcategorization.find_by_user_id_and_subcategory_id(@user.id, s.id)
