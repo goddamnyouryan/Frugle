bb4cd663739eb649dd3a55682fe30aed91e615a0
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-04-14 01:08:39 -0700

    damn you IE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout "application"
-  before_filter :fix_ct
-  
-    def fix_ct
-      headers["Content-Type"] = "text/html" 
-    end
 end
