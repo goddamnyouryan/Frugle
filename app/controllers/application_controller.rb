c8f97e1ce51e15d49a27ec4563b9830166c5606d
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-04-14 01:04:09 -0700

    Grr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout "application"
+  before_filter :fix_ct
+  
+    def fix_ct
+      headers["Content-Type"] = "text/html" 
+    end
 end
