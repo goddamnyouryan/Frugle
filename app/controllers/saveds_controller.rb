c3b07185d0b959f4b99c65460c08341489a88e4f
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-05-11 00:28:00 -0700

    added redirect after you attempt to save a frugle

--- app/controllers/saveds_controller.rb
+++ app/controllers/saveds_controller.rb
@@ -1,13 +1,18 @@
 class SavedsController < ApplicationController
   
   def new
-    @follow = Saved.create(:user_id => current_user.id, :frugle_id => params[:frugle_id])
     @frugle = Frugle.find params[:frugle_id]
-    render :update do |page|
-      page.select(".save_#{@frugle.id}").each do |element| 
-        page.replace_html element, :partial => 'frugles/save', :frugle => @frugle
+    if user_signed_in?
+      @follow = Saved.create(:user_id => current_user.id, :frugle_id => params[:frugle_id])
+      render :update do |page|
+        page.select(".save_#{@frugle.id}").each do |element| 
+          page.replace_html element, :partial => 'frugles/save', :frugle => @frugle
+	      end
 	    end
-	  end
+    else
+      session[:user_return_to] = business_path(@frugle.business)
+      redirect_to new_user_session_path, :notice => "You need to be logged in to do that"
+    end
   end
 
   def destroy
