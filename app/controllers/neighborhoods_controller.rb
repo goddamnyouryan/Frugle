b66c61a050f7549f26c79ef3edcb4475de9b73c3
Ryan MacInnes <ryan.macinnes@gmail.com>
2011-05-17 23:13:18 -0700

    fixing admin

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -93,6 +93,7 @@ class NeighborhoodsController < ApplicationController
       if current_user.role == "admin"
         @neighborhood = Neighborhood.new
         @neighborhoods = Neighborhood.all
+        layout => "splash"
       else
         redirect_to root_path
       end
